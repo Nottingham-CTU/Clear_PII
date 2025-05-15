d46a8e961fb3c985db2c28f7ac893cced6b72d52
ninaclayton <65402437+ninaclayton@users.noreply.github.com>
2025-05-15 10:08:32 +0100

    Updated filepath

--- tests/init.py
+++ tests/init.py
@@ -29,7 +29,7 @@ class Test1Login():
     dropdown.find_element(By.XPATH, "//option[. = 'Operational Support']").click()
     self.driver.find_element(By.ID, "project_template_radio2").click()
     self.driver.find_element(By.NAME, "odm").click()
-    self.driver.find_element(By.NAME, "odm").send_keys("C:\\REDCap_test_Projects\\ClearPIITest_REDCap.xml")
+    self.driver.find_element(By.NAME, "odm").send_keys("REPODIR/tests/ClearPIITest_REDCap.xml")
     time.sleep(1)
     self.driver.find_element(By.CSS_SELECTOR, ".btn-primaryrc").click()
     self.driver.set_window_size(1078, 582)
