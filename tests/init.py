360fccd1db3cbf03e56529d8443dea3b8a8bfc0e
ninaclayton <65402437+ninaclayton@users.noreply.github.com>
2025-05-19 12:18:58 +0100

    add print image

--- tests/init.py
+++ tests/init.py
@@ -103,6 +103,7 @@ class Test1Login():
     dropdown.find_element(By.XPATH, "//option[. = 'oth_pi_id - Extra PII Identifier']").click()
     self.driver.find_element(By.NAME, "clear-disc-pii-logic").click()
     self.driver.find_element(By.NAME, "clear-disc-pii-logic").send_keys(" round(datediff([screening_arm_1][eoi_date], \"today\", \"d\",\"dmy\", true)) >= 1 AND [baseline_arm_1][oth_pi_yes_no] <> 1")
+    print("ECHOSCREENSHOT")
     self.driver.find_element(By.CSS_SELECTOR, ".modal-lg .save").click()
     time.sleep(5)
     self.driver.find_element(By.LINK_TEXT, "User Rights").click()
