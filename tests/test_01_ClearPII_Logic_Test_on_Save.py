4f7b38355e1d8ee9993ff01b13d27da3819d795d
ninaclayton <65402437+ninaclayton@users.noreply.github.com>
2025-05-22 21:59:22 +0100

    test updated

--- tests/test_01_ClearPII_Logic_Test_on_Save.py
+++ tests/test_01_ClearPII_Logic_Test_on_Save.py
@@ -57,10 +57,7 @@ class Test01ClearPIILogicTestonSave():
     self.driver.execute_script("$(\'[name=\"eoi_date\"]\').val(\'\')")
     self.driver.find_element(By.NAME, "eoi_date").send_keys("08-04-2025")
     self.driver.find_element(By.NAME, "submit-btn-saverecord").click()
-    WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.ID, "footer")))
-    self.driver.execute_script("$(\'#footer\').remove()")
-    self.driver.execute_script("window.history.back()")
-    WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.ID, "footer")))
+    self.driver.execute_script("window.location.href=http://127.0.0.1/")
     self.driver.find_element(By.LINK_TEXT, "My Projects").click()
     elements = self.driver.find_elements(By.XPATH, "//*[@id=\"table-proj_table\"][contains(.,\'Clear PII Test\')]")
     assert len(elements) > 0
